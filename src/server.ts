2a0a7467209520d317b668d665ebd379f05a513d
mac <mac@MacBook-Pro-de-mac.local>
2020-03-05 23:05:45 -0600

    refactors the server to use classes

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,57 @@
+import { Express } from 'express';
 import * as express from 'express';
+import * as Debug from 'debug';
 import * as db from './db';
 import * as bodyParser from 'body-parser';
 
-export const app = express();
+const debug = Debug('app:Server');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// export const app = express();
 
-app.get('/artists', (req, res) => {
-    res.json({
-        data: db.artist.getBy()
-    });
-});
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/artists/:id', (req, res) => {
-    const { id } = req.params;
-    const [artist] = db.artist.getBy('id', id);
-    res.json({
-        data: {
-            artist
-        }
-    });
-});
+// app.get('/artists', (req, res) => {
+//     res.json({
+//         data: db.artist.getBy()
+//     });
+// });
 
-app.post('/artists/', (req, res) => {
-    const payload = req.body;
-    db.artist.post(payload);
-    res.json(payload);
-});
+// app.get('/artists/:id', (req, res) => {
+//     const { id } = req.params;
+//     const [artist] = db.artist.getBy('id', id);
+//     res.json({
+//         data: {
+//             artist
+//         }
+//     });
+// });
+
+// app.post('/artists/', (req, res) => {
+//     const payload = req.body;
+//     db.artist.post(payload);
+//     res.json(payload);
+// });
+
+export class Server {
+    public app: Express;
+
+    constructor(private port) {}
+
+    public async init() {
+        await this._initApp()
+    }
+
+    private async _initApp(): Promise<void> {
+        const {
+            port
+        } = this;
+        return new Promise((resolve) => {
+            this.app = express();
+            this.app.listen(port, () => {
+                debug(`App running on port: ${port}`);
+                resolve();
+            });
+        });
+    }
+}
